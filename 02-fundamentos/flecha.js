14b9d23d31836f76d065d9cca55a15f25f8f07d9
fedeemilo <fedeemilo91@gmail.com>
2020-04-17 20:09:42 -0300

    fundamentos -> callbacks

--- 02-fundamentos/flecha.js
+++ 02-fundamentos/flecha.js
@@ -13,5 +13,15 @@
 // Ya que el cuerpo de la función se escribió en una sola línea, puede ser acotado así:
 let sumar = (a, b) => a + b;
 
+let deadpool = {
+    nombre: 'Wade',
+    apellido: 'Winston',
+    poder: 'Regeneración',
+    getNombre()  {
+        return `${this.nombre} ${this.apellido} - Poder: ${this.poder}`
+    }
+}
+
 
 console.log(sumar(10, 20));
+console.log(deadpool.getNombre())
